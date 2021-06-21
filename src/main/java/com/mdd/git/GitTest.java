576707c6d2fcfcd3288eed41df8723bc65588dc2
alun <alun_one@163.com>
2021-06-21 21:03:09 +0800

    hot-fix commit1

--- src/main/java/com/mdd/git/GitTest.java
+++ src/main/java/com/mdd/git/GitTest.java
@@ -2,6 +2,7 @@ package com.mdd.git;
 
 public class GitTest {
     public static void main(String[] args) {
+        System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
     }
